486fd24cf46395012d7eea22d6762072d85a8f17
Tristan Strathearn <tristan@pixls.com.au>
2023-06-29 10:21:23 +1000

    feat: add site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import './globals.css'
 import { Fira_Mono } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const fira = Fira_Mono({ subsets: ['latin'], weight: ['400', '700'] })
 
-export const metadata = {
-  title: 'Descent',
-  description: 'Descent typing game',
+export const metadata: Metadata = {
+  title: 'Descent Type Training',
+  description: 'Practice typing the most common English words to build burst speed and muscle memory.',
+  openGraph: {
+    type: 'website',
+    title: 'Descent Type Training',
+    description: 'Practice typing the most common English words to build burst speed and muscle memory.',
+    images: ['https://descent-typing.vercel.app/images/descent.jpg'],
+  },
 }
 
 export default function RootLayout({
