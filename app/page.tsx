3b3c30e0076ac6ab5d1302e5a5fddac8134a068b
Tristan Strathearn <tristan@pixls.com.au>
2023-07-01 20:46:57 +1000

    chore: apply general UI enhancements

--- app/page.tsx
+++ app/page.tsx
@@ -183,7 +183,7 @@ export default function Home() {
             <button
               key={index}
               type="button"
-              className={`flex flex-col items-center w-16 py-3 border-2 rounded-md ${wpm === state.targetWPM ? 'border-green-500 text-green-400 bg-green-950 shadow-md shadow-green-800' : 'border-neutral-700 text-neutral-400'} hover:border-green-500 hover:text-green-400`}
+              className={`relative flex flex-col items-center w-16 py-3 border-2 rounded-md ${wpm === state.targetWPM ? 'border-green-500 text-green-400 bg-green-950 after:content-[""] after:absolute after:bottom-0 after:-mb-3 after:border-green-500 after:w-1/3 after:border-b-4 after:rounded-full' : 'border-neutral-700 text-neutral-400'} hover:border-green-500 hover:text-green-400`}
               onClick={() => dispatch({type: 'SET_TARGET_WPM', payload: wpm})}
             >
               <span className="font-bold text-lg">{wpm}</span>
@@ -195,7 +195,7 @@ export default function Home() {
             <button
               key={index}
               type="button"
-              className={`flex flex-col items-center w-16 py-3 border-2 rounded-md ${streak === state.targetStreak ? 'border-sky-400 text-sky-400 bg-sky-950 shadow-md shadow-sky-800' : 'border-neutral-700 text-neutral-400'} hover:border-sky-400 hover:text-sky-400`}
+              className={`relative flex flex-col items-center w-16 py-3 border-2 rounded-md ${streak === state.targetStreak ? 'border-sky-400 text-sky-400 bg-sky-950 after:content-[""] after:absolute after:bottom-0 after:-mb-3 after:border-sky-500 after:w-1/3 after:border-b-4 after:rounded-full' : 'border-neutral-700 text-neutral-400'} hover:border-sky-400 hover:text-sky-400`}
               onClick={() => dispatch({type: 'SET_TARGET_STREAK', payload: streak})}
             >
               <span className="font-bold text-lg">{streak}</span>
@@ -205,7 +205,7 @@ export default function Home() {
           <div className="h-10 border-l-2 border-neutral-800 mx-4"/>
           <button
             type="button"
-            className="flex flex-col items-center w-16 py-3 border-2 border-neutral-700 text-neutral-400 hover:border-green-500 hover:bg-green-950 hover:text-green-400 hover:shadow-md hover:shadow-green-800 rounded-md"
+            className="flex flex-col items-center w-16 py-3 border-2 border-neutral-700 text-neutral-400 hover:border-green-500 hover:text-green-400 rounded-md"
             onClick={handleSave}
           >
             <span className="font-bold text-lg">S</span>
@@ -213,7 +213,7 @@ export default function Home() {
           </button>
           <button
             type="button"
-            className="flex flex-col items-center w-16 py-3 border-2 border-neutral-700 text-neutral-400 hover:border-red-500 hover:bg-red-950 hover:text-red-400 hover:shadow-md hover:shadow-red-800 rounded-md"
+            className="flex flex-col items-center w-16 py-3 border-2 border-neutral-700 text-neutral-400 hover:border-red-500 hover:text-red-400 rounded-md"
             onClick={handleReset}
           >
             <span className="font-bold text-lg">R</span>
@@ -242,17 +242,17 @@ export default function Home() {
         </div>
       </div>
       {state.showInstructions && (
-        <div className="fixed inset-0 flex justify-center items-center bg-neutral-900 bg-opacity-70 backdrop-blur-md">
+        <div className="fixed inset-0 flex justify-center items-center bg-neutral-900 bg-opacity-60 backdrop-blur-md">
           <div className="text-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-32 h-32 stroke-yellow-400 mx-auto">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
             </svg>
-            <h1 className="mt-6 text-7xl font-bold text-neutral-50">BurstType<span className="text-blue-500">Pro</span></h1>
+            <h1 className="mt-6 text-7xl font-bold text-green-500">Burst<span className="text-white">Type</span></h1>
             <p className="mt-16 text-neutral-400 max-w-3xl mx-auto">Set your desired min <span className="text-green-400">WPM</span> and <span className="text-sky-400">streak</span> count, then type the word you see on the screen. If you complete the word with no mistakes (and at or above your min WPM setting), your streak will increase; otherwise, your streak will reset.</p>
             <p className="mt-4 text-neutral-400 max-w-3xl mx-auto">Pressing the <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">spacebar</span> after you have started typing a word will reset the word and your streak. Pressing the <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">spacebar</span> after you have successfully completed a word/streak will unlock (discover) the next word in the list.</p>
             <p className="mt-4 text-neutral-400 max-w-3xl mx-auto">You can use the <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">left/down</span> or <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">right/up</span> arrow keys to move backwards and forwards through your discovered words. Jump to back to the start or to your latest discovered word using the <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">home</span> or <span className="bg-neutral-400 text-neutral-950 font-bold px-2 py-0.5 rounded-md">end</span> keys.</p>
-            <button className="mt-16 bg-green-600 hover:bg-green-500 text-black font-bold px-4 py-2 rounded-md shadow-md shadow-green-800" type="button" onClick={() => dispatch({type: 'TOGGLE_INSTRUCTIONS'})}>Get Started!</button>
+            <button className="mt-16 bg-green-600 hover:bg-green-500 text-black font-bold px-5 py-3 rounded-md" type="button" onClick={() => dispatch({type: 'TOGGLE_INSTRUCTIONS'})}>Get Started!</button>
           </div>
         </div>
       )}
