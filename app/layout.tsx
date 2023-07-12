ca19cf3d72d63f8f1eca0912d59f60a0f6bcb4a5
Tristan Strathearn <tristan@pixls.com.au>
2023-07-12 11:24:24 +1000

    chore: add social and fav icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ const fira = Fira_Mono({subsets: ['latin'], weight: ['400', '700']});
 export const metadata: Metadata = {
 	title: 'Burst Type',
 	description: 'Practice typing the most common words to build burst speed and muscle memory.',
+	icons: {
+		icon: [
+			{url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+			{url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+		],
+		apple: '/apple-touch-icon.png',
+	},
+	manifest: '/site.webmanifest',
 	openGraph: {
 		type: 'website',
 		title: 'Burst Type',
