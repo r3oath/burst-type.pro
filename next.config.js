22450f9ce8d891cdd27b1f0b75ec2f543d2b5b72
Tristan Strathearn <tristan@pixls.com.au>
2024-02-15 20:22:33 +1100

    chore: remove legacy migrate page and replace with a permanent redirect to the home page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable unicorn/prefer-module */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/migrate',
+				destination: '/',
+				permanent: true,
+			},
+		];
+	},
+};
 
 module.exports = nextConfig;
